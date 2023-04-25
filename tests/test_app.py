a6aac5ff54c92ab59c5433652d13edbe6430181c
giraycoskun <giraycoskun@sabanciuniv.edu>
2023-04-25 23:44:26 +0300

    ci: :test_tube: Add test github action flow

---
+++ tests/test_app.py
@@ -0,0 +1,15 @@
+import pytest
+from fastapi.testclient import TestClient
+
+from src.main import app
+
+@pytest.fixture(scope="module")
+def test_app():
+    client = TestClient(app)
+    yield client  # The test runs here
+    # Clean up code, if any
+
+def test_read_main(test_app):
+    response = test_app.get("/")
+    assert response.status_code == 200
+    assert response.json() == {"Hello": "World"}
