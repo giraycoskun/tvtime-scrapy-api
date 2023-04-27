ee5bc386b4aaedd1e798f11b1857bec4ba9f904f
giraycoskun <giraycoskun@sabanciuniv.edu>
2023-04-27 13:30:50 +0300

    fix: :bug: Fix test bug

--- tests/test_service.py
+++ tests/test_service.py
@@ -5,7 +5,3 @@ from src.service.spider import TVTimeSpider
 def test_spider():
     spider = TVTimeSpider()
     assert spider.name == "tvtime"
-    assert spider.urls == [
-        "https://www.tvtime.com/en/upcoming", 
-        "https://www.tvtime.com/en/to-watch"
-    ]
