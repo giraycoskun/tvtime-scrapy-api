703e633cecf83f3f860909209b0722eb5842e03c
giraycoskun <giraycoskun@sabanciuniv.edu>
2023-04-30 20:19:48 +0300

    fix: :bug: Fix test bug & Update openapi spec

--- tests/test_service.py
+++ tests/test_service.py
@@ -1,7 +1,9 @@
 import pytest
 
 from src.repository.spider import TVTimeSpider
+from src.repository.models import TVTimeUser
 
 def test_spider():
-    spider = TVTimeSpider()
+    user = TVTimeUser(username="test", password="test")
+    spider = TVTimeSpider(user)
     assert spider.name == "tvtime"
