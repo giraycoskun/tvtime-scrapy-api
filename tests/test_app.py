2e841b244cead4790fafe5db4cd6dda3db085e6e
giraycoskun <giraycoskun@sabanciuniv.edu>
2023-05-02 16:10:18 +0300

    test: :bug: Delete old test

--- tests/test_app.py
+++ tests/test_app.py
@@ -15,10 +15,6 @@ def test_read_root(test_app):
     assert response.status_code == 200
     assert response.json() == {"Hello": "World"}
 
-def test_get_watch_next(test_app):
-    response = test_app.get("/tvtime/watch-next")
-    assert response.status_code == 201
-
 def test_api_spec(test_app):
     write_openapi_spec(app)
     assert exists("./docs/assets/openapi.json")
