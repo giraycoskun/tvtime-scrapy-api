b7bb41684a7a99c1f1cdb097c0014b6b2aa485e6
giraycoskun <giraycoskun@sabanciuniv.edu>
2023-05-04 16:50:15 +0300

    feat: :card_file_box: Update redis client class

--- src/repository/redis_repository.py
+++ src/repository/redis_repository.py
@@ -1,4 +1,3 @@
-import redis
 from loguru import logger
 from redis_om import get_redis_connection, Migrator, MigrationError, NotFoundError
 
@@ -6,52 +5,44 @@ from src.config import REDIS_URL
 from src.repository.models import TVTimeDataModel
 
 
-class RedisClient:
-    def __init__(self):
-        self._connection = redis.Redis()
-        response = self._connection.ping()
-        logger.info(f"Redis Connection: {response}")
-
-    def set_user(self, username):
-        self._connection.set('user', username)
-
 
 class RedisOMClient:
     def __init__(self) -> None:
         self.redis = get_redis_connection(url=REDIS_URL)
         TVTimeDataModel.Meta.database = self.redis
-        try:
-            Migrator().run()
-        except MigrationError as e:
-            logger.error(e)
+        # try:
+        #     Migrator().run()
+        # except MigrationError as e:
+        #     logger.error(e)
 
-    def exists(self, username):
-        try:
-            TVTimeDataModel.get(username)
-            return True
-        except NotFoundError:
-            return False
+    def exists(self, key):
+        key = f"tvtime:data:{key}"
+        return self.redis.exists(key)
+        
+    def get_ttl(self, key):
+        key = f"tvtime:data:{key}"
+        return self.redis.ttl(key)
 
-    def get_tvtime_data(self, username):
-        user = TVTimeDataModel.get(username)
+    def get_tvtime_data(self, key):
+        user = TVTimeDataModel.get(key)
         return user
 
-    def get_tvtime_towatch(self, username):
-        data = TVTimeDataModel.get(username)
+    def get_tvtime_watch_next(self, key):
+        data = TVTimeDataModel.get(key)
         if 'Watch next' in data.to_watch:
             return data.to_watch['Watch next']
         else:
             return {}
 
-    def get_tvtime_not_watched_for_while(self, username):
-        data = TVTimeDataModel.get(username)
+    def get_tvtime_not_watched_for_while(self, key):
+        data = TVTimeDataModel.get(key)
         if 'Not watched for a while' in data.to_watch:
             return data.to_watch['Not watched for a while']
         else:
             return {}
         
-    def get_tvtime_not_started_yet(self, username):
-        data = TVTimeDataModel.get(username)
+    def get_tvtime_not_started_yet(self, key):
+        data = TVTimeDataModel.get(key)
         if 'Not started yet' in data.to_watch:
             return data.to_watch['Not started yet']
         else:
