76f5b8ba172d6924102358080745ec6eed5207ae
giraycoskun <giraycoskun@sabanciuniv.edu>
2023-05-05 20:07:07 +0300

    feat: :sparkles: Add request repository for update/delete routes

--- src/config.py
+++ src/config.py
@@ -6,3 +6,13 @@ load_dotenv()
 ENVIRONMENT = os.getenv("ENVIRONMENT")
 
 REDIS_URL = os.getenv("REDIS_URL")
+
+USER_AGENT = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/105.0.0.0 Safari/537.36"
+TVTIME_HOST = "www.tvtime.com"
+TVTIME_URL = "https://www.tvtime.com/"
+TVTIME_SIGNIN_URL = "https://www.tvtime.com/signin"
+TVTIME_SIGNOUT_URL = "https://www.tvtime.com/signout"
+TVTIME_TOWATCH_URL = "https://www.tvtime.com/en/to-watch"
+
+TVTIME_USERNAME = os.getenv("TVTIME_USERNAME")
+TVTIME_PASSWORD = os.getenv("TVTIME_PASSWORD")
---
+++ src/repository/request_repositoy.py
@@ -0,0 +1,99 @@
+from requests import Session
+from loguru import logger
+
+from src.repository.models import TVTimeUser
+from src.config import TVTIME_URL, TVTIME_SIGNIN_URL, TVTIME_SIGNOUT_URL, TVTIME_TOWATCH_URL, TVTIME_HOST, USER_AGENT
+from src.config import TVTIME_USERNAME, TVTIME_PASSWORD
+
+
+class TVTimeClient:
+
+    def __init__(self, user: TVTimeUser) -> None:
+        self.user = user
+        self.session = Session()
+        headers = {
+            "User-Agent": USER_AGENT,
+            "Host": TVTIME_HOST
+        }
+        self.session.headers.update(headers)
+
+    def login(self):
+        payload = {
+            'username': self.user.username,
+            'password': self.user.password,
+            'redirect_path': TVTIME_TOWATCH_URL
+        }
+        self.session.post(TVTIME_SIGNIN_URL, data=payload)
+
+    def show_seasons(self, show_id):
+        url = f"{TVTIME_URL}show_seasons?show_id={show_id}"
+        response = self.session.get(url)
+        logger.debug("Show Seasons: {response}", response=response.status_code)
+        logger.debug("Show Seasons: {response}", response=response.text)
+        return response
+
+    def mark_show_followed(self, show_id, follow=True):
+        url = f"{TVTIME_URL}followed_shows"
+        data = {'show_id': show_id}
+        if follow:
+            response = self.session.put(url=url, data=data)
+        else:
+            response = self.session.delete(url=url, data=data)
+        logger.debug("Follow Show: {response}", response=response.status_code)
+        logger.debug("Follow Show: {response}", response=response.text)
+        return response
+
+    def mark_season_watched(self, show_id, season, watched=True):
+        url = f"{TVTIME_URL}watched_season"
+        data = {'show_id': show_id, 'season': season}
+        if watched:
+            response = self.session.put(url=url, data=data)
+        else:
+            response = self.session.delete(url=url, data=data)
+        logger.debug("Watched Season: {response}",
+                     response=response.status_code)
+        logger.debug("Watched Season: {response}", response=response.text)
+
+    def mark_episode_watched(self, episode_id, watched=True):
+        url = f"{TVTIME_URL}watched_episodes"
+        data = {'episode_id': episode_id}
+        if watched:
+            response = self.session.put(url=url, data=data)
+        else:
+            response = self.session.delete(url=url, data=data)
+        logger.debug("Watched Episode: {response}",
+                     response=response.status_code)
+        logger.debug("Watched Episode: {response}", response=response.text)
+
+    def mark_show_until_watched(self, season, episode, show_id):
+        url = f"{TVTIME_URL}show_watch_until"
+        data = {
+            'season': season,
+            'episode': episode,
+            'show_id': show_id
+        }
+        response = self.session.put(url=url, data=data)
+        logger.debug("Watched Until: {response}", response=response.status_code)
+        logger.debug("Watched Until: {response}", response=response.text)
+
+    def get_show_ratings(self, show_id):
+        url = f"{TVTIME_URL}show/{show_id}/ratings"
+        response = self.session.get(url)
+        logger.debug("Show Ratings: {response}", response=response.status_code)
+        logger.debug("Show Ratings: {response}", response=response.text)
+        return response
+
+    def logout(self):
+        logger.debug("Logout TVTimeClient")
+        self.session.get(TVTIME_SIGNOUT_URL)
+        self.session.close()
+
+
+if __name__ == "__main__":
+    user = TVTimeUser(username=TVTIME_USERNAME, password=TVTIME_PASSWORD)
+    tv_time_client = TVTimeClient(user)
+    tv_time_client.login()
+    tv_time_client.get_show_ratings(394290)
+    # tv_time_client.mark_episode_watched(8112670, watched=False)
+    # tv_time_client.show_seasons(394290)
+    tv_time_client.logout()
